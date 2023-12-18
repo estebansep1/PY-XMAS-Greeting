2f5e0841a1d9772ac183dda5693d57507e56e183
Esteban Sepulveda <tebansd@icloud.com>
2023-12-18 14:43:43 -0800

    changed name of functions to make a little more sense

--- christmas_message.py
+++ christmas_message.py
@@ -1,25 +1,25 @@
 import time
 
-def print_welcome_message():
+def print_welcome_greeting():
     print("🎄🎅🏻 Welcome to annual the Christmas Greeting")
 
 def get_user_name():
     name = input("What's your name? ")
     return name
 
-def print_xmas_greeting(name):
+def print_xmas_message(name):
     print("\nHi " + name + "!")
     time.sleep(1)
-    print("Wishing you a amagical and joyous Christmas")
+    print("Wishing you a magical and joyous Christmas")
     time.sleep(1)
     print("May your days be merry and bright!")
     time.sleep(1)
     print("Merry christmas, " + name + "!")
 
 def main():
-    print_welcome_message()
+    print_welcome_greeting()
     user_name = get_user_name()
-    print_xmas_greeting(user_name)
+    print_xmas_message(user_name)
 
 if __name__ == "__main__":
     main() 
